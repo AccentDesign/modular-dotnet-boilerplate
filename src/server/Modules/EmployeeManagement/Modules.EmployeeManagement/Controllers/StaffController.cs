0a6937c0cf982b43c105f783946039001f687146
reza bashiri <rzbashiri@gmail.com>
2022-09-16 17:56:27 +0430

    add base controler and a basic one to support staff action

---
+++ src/server/Modules/EmployeeManagement/Modules.EmployeeManagement/Controllers/StaffController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Modules.EmployeeManagement.Core.Features.Staff.Commands;
+using Shared.Infrastructure.Controllers;
+
+namespace Modules.EmployeeManagement.Controllers
+{
+    [ApiVersion("1")]
+    public class StaffController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<ActionResult> EmployStaffAsync(EmployStaffCommand command)
+        {
+            return StatusCode((int)HttpStatusCode.Created, await Mediator.Send(command));
+        }
+    }
+}
